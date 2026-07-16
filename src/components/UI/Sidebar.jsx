8f7aeb98b32fa4572995c33f2cb63818efa617be
Mansij19 <mansij.1930@gmail.com>
2026-07-17 00:30:43 +0530

    right side updated

--- src/components/UI/Sidebar.jsx
+++ src/components/UI/Sidebar.jsx
@@ -8,35 +8,64 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 const Sidebar = () => {
   const data =[{
-    icon : <ForumIcon/>
+    icon : <ForumIcon/>,
     value : "All Feed"
   },
   {
-    icon : <HelpCenterIcon/>
+    icon : <HelpCenterIcon/>,
     value : "Questions"
   },
   {
-    icon : <LibraryBooksIcon/>
+    icon : <LibraryBooksIcon/>,
     value : "Answers"
   },
   {
-    icon : <GroupsIcon/>
+    icon : <GroupsIcon/>,
     value : "Study Groups"
   },{
-    icon : <CampaignIcon/>
+    icon : <CampaignIcon/>,
     value : "Announcements"
   },
-  {
-    icon : <SettingsIcon/>
+  
+];
+const dataNext =[{
+    icon : <SettingsIcon/>,
     value : "Settings"
   },
   {
-    icon : <QuestionMarkIcon/>
+    icon : <QuestionMarkIcon/>,
     value : "Help"
-  }
-]
+  }]
   return (
-    <div>Sidebar</div>
+    <div className='w-full'>
+      <div className='border border-black'>
+        {data.map((item, index) => (
+          <button
+            key={index}
+            className={`w-full flex items-center gap-3 px-4 py-2 rounded-lg text-sm font-medium transition-all duration-200
+              ${
+                item.active
+                  ? "bg-blue-600 text-white"
+                  : "text-gray-700 hover:bg-gray-100"
+              }`}
+          >
+            {item.icon}
+            <span>{item.value}</span>
+          </button>
+        ))}
+      </div>
+       <div className="border border-black space-y-2">
+        {dataNext.map((item, index) => (
+          <button
+            key={index}
+            className="w-full flex items-center gap-3 px-4 py-2 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-100 transition-all duration-200"
+          >
+            {item.icon}
+            <span>{item.value}</span>
+          </button>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Sidebar from '../components/UI/Sidebar'
 
 const CommunityPage = () => {
+  const sort = [
+    "Top Rated",
+    "Most Recent",
+    "Most active",
+  ]
   return (
     <div className='flex w-full h-screen '>
       {/* left side */}
       <div className='w-[15%] h-full border border-red-400'>
-        <div className='w-full flex flex-col justify-centre items-centre'>
+        <div className='w-full flex flex-col '>
           <div className='flex flex-col p-2 gap-1 border border-pink-600'>
             <div className='text-2xl text-blue-600 font-semibold'>Community Hub</div>
             <div>Connect, share and grow together </div>
@@ -15,11 +21,39 @@ const CommunityPage = () => {
               Start Discussion
             </button>
           </div>
+          <div className='flex flex-col justify-between'>
+            <Sidebar/>
+          </div>
         </div>
       </div>
       {/* right side */}
-      <div className='w-[85%] h-full border border-green-500'>
-        
+      <div className='w-[85%] h-full border border-green-500 '>
+        <div className='flex flex-col w-full border px-2 py-3 border-orange-500'>
+          <div className='text-3xl'>Popular Discussion</div>
+          <div className='flex border border-black justify-between items-center-safe'>
+            <div className='flex border border-amber-400'>Trending topics in the learning community right now.
+            </div>
+            <div className='flex border border-red-400 gap-x-3'>
+            <div>SORT BY:</div>
+            <div>
+              <select
+            name=""
+            id=""
+            className="border rounded-md  w-full"
+          >
+            <option value="">Top Rated</option>
+
+            {sort.map((rate, index) => (
+              <option key={index} value={rate}>
+                {rate}
+              </option>
+            ))}
+          </select>
+            </div>
+          </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   )
